659b2a3f5a9fe48d4c048f078b376764147eaa25
iw.jhun <dlfdnd0725@gmail.com>
2017-09-14 13:33:04 +0900

    cc

--- src/main/java/com/glqdlt/crawling/domain/CoolenjoyNews.java
+++ src/main/java/com/glqdlt/crawling/domain/CoolenjoyNews.java
@@ -2,11 +2,11 @@ package com.glqdlt.crawling.domain;
 
 public class CoolenjoyNews {
 
-	public static int site_tag = 2;
-	public static int data_tag = 3;
-	public static String site_name = "쿨엔조이";
-	public static String data_name = "뉴스 정보";
-	public static String target_url = "http://www.coolenjoy.net/bbs/38";
-	public static int craw_type = 1;
+	final public static int site_tag = 2;
+	final public static int data_tag = 3;
+	final public static String site_name = "쿨엔조이";
+	final public static String data_name = "뉴스 정보";
+	final public static String target_url = "http://www.coolenjoy.net/bbs/38";
+	final public static int craw_type = 1;
 
 }
--- src/main/java/com/glqdlt/crawling/domain/YepannetTukga.java
+++ src/main/java/com/glqdlt/crawling/domain/YepannetTukga.java
@@ -2,10 +2,10 @@ package com.glqdlt.crawling.domain;
 
 public class YepannetTukga {
 
-	public static String site_name = "예판넷";
-	public static int site_tag = 3;
-	public static String data_name = "특가 정보";
-	public static int data_tag = 1;
-	public static String target_url = "http://yepan.net/bbs/board.php?bo_table=comm_info&sca=&sca=%ED%8A%B9%ED%8C%90%EC%A0%95%EB%B3%B4";
-	public static int craw_type = 1;
+	final public static String site_name = "예판넷";
+	final public static int site_tag = 3;
+	final public static String data_name = "특가 정보";
+	final public static int data_tag = 1;
+	final public static String target_url = "http://yepan.net/bbs/board.php?bo_table=comm_info&sca=&sca=%ED%8A%B9%ED%8C%90%EC%A0%95%EB%B3%B4";
+	final public static int craw_type = 1;
 }
